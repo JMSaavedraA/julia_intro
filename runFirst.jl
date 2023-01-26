c33383cf0d4b490211ed9d728f4520457b9ed087
José Miguel Saavedra <jose.saavedra@cimat.mx>
2023-01-25 22:15:42 -0600

    Eliminada References y archivos no utiliados

--- runFirst.jl
+++
@@ -1,7 +0,0 @@
-# script para instalar Plots en Julia
-using Pkg
-println("Instalando paquetes necesarios para graficar")
-Pkg.update()
-Pkg.add("Plots")
-Pkg.add("LaTeXStrings")
-@warn("Packages installed!")
